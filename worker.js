8911d075a82934d017c046fa41f9e9633ed69d55
MohammadHossein Sabourifar <mh.sabourifar@gmail.com>
2025-11-08 21:50:49 +0330

    Update worker.js
    no need to set Host, fetch handles it

--- worker.js
+++ worker.js
@@ -2,23 +2,19 @@
 // DOH Proxy by Sabourifar
 // GitHub: https://github.com/Sabourifar/DOHProxy
 // ==========================================
-
 const DOH_ENDPOINT = "https://cloudflare-dns.com/dns-query";
-
 // Export worker module with fetch handler
 export default {
-	async fetch(request) {
-		// Extract query parameters from incoming request
-		const url = new URL(request.url);
-		// Clone headers and update Host to match DoH server
-		const headers = new Headers(request.headers);
-		headers.set("Host", "cloudflare-dns.com");
-		
-		// Forward request to Cloudflare DoH with corrected headers
-		return fetch(DOH_ENDPOINT + url.search, {
-			method: request.method,
-			headers: headers,
-			body: request.body,
-		});
-	},
+  async fetch(request) {
+    // Extract query parameters from incoming request
+    const url = new URL(request.url);
+    // Clone headers (no need to set Host, fetch handles it)
+    const headers = new Headers(request.headers);
+    // Forward request to Cloudflare DoH with corrected headers
+    return fetch(DOH_ENDPOINT + url.search, {
+      method: request.method,
+      headers: headers,
+      body: request.body,
+    });
+  },
 };
