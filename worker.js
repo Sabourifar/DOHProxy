f8aca511c7b115ab964bc5d7c630d5601b872d76
MohammadHossein Sabourifar <mh.sabourifar@gmail.com>
2025-11-24 17:54:31 +0330

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,12 +1,10 @@
 // DOH Proxy by Sabourifar - GitHub: https://github.com/Sabourifar/DOHProxy
 // Forwards DNS-over-HTTPS requests to Cloudflare. To use a different DoH provider, replace the https://cloudflare-dns.com/dns-query below.
 export default {
-	fetch(request) {
-		const url = new URL(request.url);
-		return fetch("https://cloudflare-dns.com" + url.pathname + url.search, {
-			method: request.method,
-			headers: request.headers,
-			body: request.body,
-		});
+	fetch(req) {
+		return fetch(
+			"https://cloudflare-dns.com/dns-query" + new URL(req.url).search,
+			req,
+		);
 	},
 };
