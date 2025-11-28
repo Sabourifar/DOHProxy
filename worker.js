11969becf7e892271c64ca46db73230520bdf504
MohammadHossein Sabourifar <mh.sabourifar@gmail.com>
2025-11-29 01:34:58 +0330

    Refactor fetch function for cleaner syntax

--- worker.js
+++ worker.js
@@ -1,8 +1,5 @@
 // DOHProxy by Sabourifar - GitHub: https://github.com/Sabourifar/DOHProxy
 // Forwards DNS-over-HTTPS requests to Cloudflare. To use a different DoH provider, replace the https://cloudflare-dns.com/dns-query below.
 export default {
-	fetch(request) {
-		const url = `https://cloudflare-dns.com/dns-query${new URL(request.url).search}`;
-		return fetch(new Request(url, request));
-	},
+  fetch: (req) => fetch(new Request('https://cloudflare-dns.com/dns-query' + new URL(req.url).search, req))
 };
