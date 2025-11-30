5537e02f2731b5bd830d727e8f57207e627a380c
MohammadHossein Sabourifar <mh.sabourifar@gmail.com>
2025-11-30 03:57:27 +0330

    Change DoH provider to Cloudflare's faster alternative

--- worker.js
+++ worker.js
@@ -1,5 +1,5 @@
 // DOHProxy by Sabourifar - GitHub: https://github.com/Sabourifar/DOHProxy
 // Forwards DNS-over-HTTPS requests to Cloudflare. To use a different DoH provider, replace the https://cloudflare-dns.com/dns-query below.
 export default {
-  fetch: (req) => fetch(new Request('https://cloudflare-dns.com/dns-query' + new URL(req.url).search, req))
+	fetch: (req) => fetch(new Request(`https://one.one.one.one/dns-query${new URL(req.url).search}`,req),),
 };
