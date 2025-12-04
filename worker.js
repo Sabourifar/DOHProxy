5202812624b7801cbb4b85ead82ff1a9cf693344
MohammadHossein Sabourifar <mh.sabourifar@gmail.com>
2025-12-04 03:40:26 +0330

    Remove search query from DoH request forwarding URL
    Removes the appending of client query parameters (+new URL(r.url).search) to the DoH provider URL. This simplifies the forwarding to only use the fixed endpoint (https://1.1.1.1/dns-query), relying on the request body (POST) or headers for the DNS query data instead of URL parameters.

--- worker.js
+++ worker.js
@@ -1,3 +1,3 @@
 // DOHProxy by Sabourifar - GitHub: https://github.com/Sabourifar/DOHProxy
 // Forwards DNS-over-HTTPS requests to Cloudflare. To use a different DoH provider, replace the URL (https://1.1.1.1/dns-query) below.
-export default{fetch:r=>fetch("https://1.1.1.1/dns-query"+new URL(r.url).search,r)}
+export default{fetch:r=>fetch("https://1.1.1.1/dns-query",r)}
