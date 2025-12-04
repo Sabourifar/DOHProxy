b651a7b93a0aa0d07eef08e6ca85888f44410e4f
MohammadHossein Sabourifar <mh.sabourifar@gmail.com>
2025-12-05 01:56:02 +0330

    refactor: replace block-style query extraction with compact ternary
    Switched from multi-line const i/indexOf/slice/return block to a single-expression ternary using includes + slice.
    
    Reduces code size, removes variables and explicit return.

--- worker.js
+++ worker.js
@@ -1,3 +1,3 @@
 // DOHProxy by Sabourifar - GitHub: https://github.com/Sabourifar/DOHProxy
 // Forwards DNS-over-HTTPS requests to Cloudflare. To use a different DoH provider, replace the URL (https://1.1.1.1/dns-query) below.
-export default{fetch:r=>{const i=r.url.indexOf('?');const query=i===-1?'':r.url.slice(i);return fetch("https://1.1.1.1/dns-query"+query,r)}}
+export default{fetch:r=>fetch("https://1.1.1.1/dns-query"+(r.url.includes("?")?r.url.slice(r.url.indexOf("?")):""),r)}
